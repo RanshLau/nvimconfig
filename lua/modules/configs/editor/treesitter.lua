9b250703e7b39875336295888cb5e06b12990c9f
刘仁秀 <ransh_liu@foxmail.com>
2024-05-30 22:13:39 +0800

    init

---
+++ lua/modules/configs/editor/treesitter.lua
@@ -0,0 +1,58 @@
+return vim.schedule_wrap(function()
+	local use_ssh = require("core.settings").use_ssh
+
+	vim.api.nvim_set_option_value("foldmethod", "expr", {})
+	vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", {})
+
+	require("modules.utils").load_plugin("nvim-treesitter", {
+		ensure_installed = require("core.settings").treesitter_deps,
+		highlight = {
+			enable = true,
+			disable = function(ft, bufnr)
+				if vim.tbl_contains({ "vim" }, ft) then
+					return true
+				end
+
+				local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
+				return ok and is_large_file
+			end,
+			additional_vim_regex_highlighting = false,
+		},
+		incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<cr>",
+        node_incremental = "<cr>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+		textobjects = {
+			select = {
+				enable = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+			move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+      },
+		},
+		indent = { enable = true },
+		matchup = { enable = true },
+	}, false, require("nvim-treesitter.configs").setup)
+	require("nvim-treesitter.install").prefer_git = true
+	-- if use_ssh then
+	-- 	local parsers = require("nvim-treesitter.parsers").get_parser_configs()
+	-- 	for _, p in pairs(parsers) do
+	-- 		p.install_info.url = p.install_info.url:gsub("https://github.com/", "git@github.com:")
+	-- 	end
+	-- end
+end)
---
+++ lua/modules/plugins/ui.lua
@@ -0,0 +1,56 @@
+local ui = {}
+
+ui["goolord/alpha-nvim"] = {
+	lazy = true,
+	event = "BufWinEnter",
+	config = require("ui.alpha"),
+}
+ui["akinsho/bufferline.nvim"] = {
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.bufferline"),
+}
+ui["sainnhe/gruvbox-material"] = {
+	lazy = false,
+	name = "gruvbox",
+	branch = "main",
+	config = require("ui.gruvbox"),
+}
+ui["j-hui/fidget.nvim"] = {
+	lazy = true,
+	event = "LspAttach",
+	config = require("ui.fidget"),
+}
+ui["lewis6991/gitsigns.nvim"] = {
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+	config = require("ui.gitsigns"),
+}
+ui["nvim-lualine/lualine.nvim"] = {
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.lualine"),
+}
+ui["rcarriga/nvim-notify"] = {
+	lazy = true,
+	event = "VeryLazy",
+	config = require("ui.notify"),
+}
+ui["folke/paint.nvim"] = {
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+	config = require("ui.paint"),
+}
+ui["folke/todo-comments.nvim"] = {
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+	config = require("ui.todo"),
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
+ui["dstein64/nvim-scrollview"] = {
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.scrollview"),
+}
+
+return ui
